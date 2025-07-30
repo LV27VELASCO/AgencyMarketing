f3ed9c148733e9362d66d896e8a3bd44fd37c8e7
Luis Velasco <luiselsw27@gmail.com>
2025-07-30 23:09:51 +0200

    first commit

---
+++ src/utils/data.ts
@@ -0,0 +1,40 @@
+const services = [
+    {
+        title: "Gestión de Redes Sociales",
+        description: "Creamos, optimizamos y gestionamos tus perfiles en las plataformas clave (Instagram, Facebook, TikTok, LinkedIn…). Publicaciones creativas, consistentes y alineadas con tu marca.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: " Publicidad en Redes (Social Ads)",
+        description: "Diseñamos campañas que convierten. Desde segmentación estratégica hasta análisis de resultados, te ayudamos a llegar al público correcto y maximizar tu retorno de inversión.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Contenido que Conecta",
+        description: "Diseñamos contenido visual y escrito que genera interacción y construye comunidad. Creatividad, storytelling y diseño con propósito.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Estrategia Digital Personalizada",
+        description: "No creemos en fórmulas mágicas. Analizamos tu negocio, tu audiencia y tu sector para crear una estrategia a medida que impulse tus objetivos reales.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Auditoría y Optimización",
+        description: "¿Ya estás en redes pero no ves resultados? Revisamos tu presencia digital y te damos un plan claro para mejorar visibilidad, engagement y conversiones.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Gestión de Comunidad (Community Management)",
+        description: "Respondemos, moderamos y generamos conversaciones valiosas con tu audiencia. Construimos relaciones auténticas entre tu marca y tus seguidores.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+];
+
+export { services };
