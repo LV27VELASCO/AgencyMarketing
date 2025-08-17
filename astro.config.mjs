a741946dcb8ff607e558fe7ebf4b009a0a672b17
Luis Velasco <luiselsw27@gmail.com>
2025-08-16 21:28:38 -0400

    standalone

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   // @ts-ignore
-  adapter: node(),
+  adapter: node({
+    mode: "standalone", // o "middleware"
+  }),
   server: {
       allowedHosts: [
         '.weeklysocial.es'
